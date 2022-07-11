73023f6e9145f94908b4328274b23f3e58625094
reinarQ <reinquartier@gmail.com>
2022-07-11 12:24:03 +0000

    removed content of WEBSITE

--- WEBSITE/cogip/view/consultoneview.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'view/navbarview.php';
-require 'view/headerview.php';
-function show($list){
-    var_dump($list);
-}
-
-require 'view/footerview.php';
-?>
--- WEBSITE/cogip/view/footerview.php
+++
@@ -1,6 +0,0 @@
-
-<footer>
-<img src="https://github.com/0ctavia/cogip/blob/development/assets/images/slogan.png?raw=true" alt="slogan 'vive la cogip'">
-</footer>
-</body>
-</html>
